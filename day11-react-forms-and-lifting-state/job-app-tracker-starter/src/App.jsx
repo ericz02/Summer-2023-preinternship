346756d6e9d9ae27fe3ed0336c5615b6d6c3d9a7
ericz02 <ezheng0720@gmail.com>
2023-06-27 11:52:15 -0400

    adds Modal

--- day11-react-forms-and-lifting-state/job-app-tracker-starter/src/App.jsx
+++ day11-react-forms-and-lifting-state/job-app-tracker-starter/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import classNames from "classnames";
 import JobCard from "./JobCard";
-import jobs from "./jobs";
+import jobsData from "./jobs";
 import "./App.css";
+import Modal from "./ui/Modal";
 
 const statuses = {
   1: "Bookmarked",
@@ -14,6 +15,8 @@ const statuses = {
 };
 
 function App() {
+  const [jobs, setJobs] = useState(jobsData);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedStatus, setSelectedStatus] = useState(1);
 
   const filteredJobs = jobs.filter(job => job.status === selectedStatus)
@@ -40,12 +43,53 @@ function App() {
     );
   })
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  }
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  }
+
+  const handleAddJobFormSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+
   return (
     <div className="mx-auto max-w-4xl">
       <h1>Job Application Tracker</h1>
       <div className="grid grid-cols-6 my-4">{statusButtons}</div>
+      <div className="flex justify-between">
+        <div></div>
+        <div>
+          <button
+            className="bg-blue-500 px-4 py-2 hover:bg-blue-600 transition"
+            onClick={showModal}
+          >
+            + Add Job
+          </button>
+        </div>
+      </div>
 
       {jobCards}
+      <Modal
+        isVisible={isModalVisible}
+        hideModal={hideModal}
+      >
+        <form onSubmit={handleAddJobFormSubmit} className="selection:bg-blue-200 flex flex-col gap-2">
+          <h1>Add Job Posting</h1>
+          <fieldset className="flex flex-col">
+            <label htmlFor="title">Job Title</label>
+            <input 
+              type="text" 
+              name="title" 
+              id="title"
+              className="bg-white border-4 focus:outline-none"
+            />
+          </fieldset>
+        </form>
+      </Modal>
     </div>
   );
 }
